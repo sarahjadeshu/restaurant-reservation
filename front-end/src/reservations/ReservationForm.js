38a5cbdc69ed10f33f4477bcc7dc4fd8c394a06c
Sarah Shu <sarahjadeshu@gmail.com>
2023-06-29 00:50:18 -0700

    first commit

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -3,20 +3,56 @@ import React from "react";
 function ReservationForm({ reservation, handleSubmit, handleCancel, handleChangge }) {
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="first_name">First Name</label>
-                    <input
-                        className="form-control"
-                        name="first_name"
-                        id="first_name"
-                        value={reservation.first_name}
-                        onChange={handleChange}
-                        required={true}
-                    />
-                </div>
-            </form>
-        </>
-    )
+      <>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="first_name">First Name</label>
+            <input
+              className="form-control"
+              name="first_name"
+              id="first_name"
+              value={reservation.first_name}
+              onChange={handleChange}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="last_name">Last Name</label>
+            <input
+              className="form-control"
+              name="last_name"
+              id="last_name"
+              value={reservation.last_name}
+              onChange={handleChange}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="mobile_number">Mobile Number</label>
+            <input
+              className="form-control"
+              name="mobile_number"
+              id="mobile_number"
+              value={reservation.mobile_number}
+              onChange={handleChange}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="reservation_date">Reservation Date</label>
+            <input
+              type="date"
+              className="form-control"
+              name="reservation_date"
+              id="reservation_date"
+              value={reservation.reservation_date}
+              placeholder="YYYY-MM-DD"
+              pattern="\d{4}-\d{2}-\d{2}"
+              onChange={handleChange}
+              required={true}
+            />
+          </div>
+        </form>
+      </>
+    );
 }
