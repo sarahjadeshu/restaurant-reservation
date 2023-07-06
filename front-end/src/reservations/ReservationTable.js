ddfa0b41bc372e4f42d1c229f21dc2af92fa902b
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-05 22:10:47 -0700

    reservation table changes

--- front-end/src/reservations/ReservationTable.js
+++ front-end/src/reservations/ReservationTable.js
@@ -8,6 +8,17 @@ function ReservationTable({ reservations, handleCancel }) {
             <td>{reservation.last_name}</td>
             <td>{reservation.people}</td>
             <td>{reservation.reservation_date}</td>
+            <td>{reservation.reservation_time}</td>
+            <td data-reservation-id-status={reservation.reservation_id}>
+                {reservation.status}
+            </td>
+            <td className="btn-group" role="group">
+                {reservation.status === "booked" && (
+                    <a href={`/reservations/${reservation.reservation_id}/edit`}
+                    type="button"
+                    className="btn btn-secondary">Edit</a>
+                )}
+            </td>
         </tr>
     ))
 }
