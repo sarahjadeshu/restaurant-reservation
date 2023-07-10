6c5ad9a298d200535949f9b6a8a3863dac1e0cb0
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-10 15:31:55 -0700

    created search component

---
+++ front-end/src/reservations/Search.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import ErrorAlert from "../layout/ErrorAlert";
+import ReservationTable from "../reservations/ReservationTable";
+import { listReservations, updateReservationState } from "../utils/api";
+
