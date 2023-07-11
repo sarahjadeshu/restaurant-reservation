b93214ffbb7d46402ab551f532274a23799edf10
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-10 21:29:22 -0700

    updated search component

--- front-end/src/reservations/Search.js
+++ front-end/src/reservations/Search.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import ErrorAlert from "../layout/ErrorAlert";
 import ReservationTable from "../reservations/ReservationTable";
-import { listReservations, updateReservationState } from "../utils/api";
+import { listReservations, updateStatus } from "../utils/api";
 
+function Search () {
+    const [error, setError] = useState(null);
+    const [mobile_number, setMobileNumber ] = useState("");
+}
