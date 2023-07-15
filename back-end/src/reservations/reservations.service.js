39bf4610613b8a0f0c64e58f2a3f945aaa1c02d3
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-14 20:38:24 -0700

    added reservations service file

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -13,3 +13,30 @@ function create(newReservation) {
     return knex("reservations").insert(newReservation).returning("*")
     .then((createdRecords) => createdRecords[0]);
 }
+
+function read(reservation_id) {
+    return knex("reservations").select("*").where({ reservation_id: reservation_id }).first();
+}
+
+async function update(updatedReservation) {
+    return await knex("reservations").select("*").where({ reservation_id: updatedReservation.reservation_id })
+    .update(updatedReservation, "*").then((updatedRecords) => updatedRecords[0]);
+}
+
+function search(mobile_number) {
+    return knex("reservations")
+    .whereRaw(
+        "translate(mobile_number, '() -', '') like ?",
+        `${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
+}
+
+module.exports = {
+    list,
+    listByDate,
+    create,
+    read,
+    update,
+    search,
+}
