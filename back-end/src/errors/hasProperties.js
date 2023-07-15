ae8d5190c8731c312b76f4faf868fbb7dae77e6b
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-15 01:46:27 -0700

    implemented hasProperties

---
+++ back-end/src/errors/hasProperties.js
@@ -0,0 +1,22 @@
+function hasProperties(...properties) {
+    return function (req, res, next) {
+        const { data = {} } = res.body;
+
+        try {
+            properties.forEach((property) => {
+                const value = data[property];
+
+                if (!value) {
+                    const error = new Error(`A '${property}' property is required.`);
+                    error.status = 400;
+                    throw error;
+                }
+            });
+            next();
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = hasProperties;
