6aef5f3d3ad9a99363c706b69e2fc6ff4f61b6ca
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-14 23:27:41 -0700

    edited createtable component

--- front-end/src/tables/CreateTable.js
+++ front-end/src/tables/CreateTable.js
@@ -31,4 +31,56 @@ function CreateTable() {
             })
             .catch(setError);
     }
+
+    return (
+        <div>
+            <h1>Create A New Table</h1>
+            <ErrorAlert error={error} />
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <div className="row">
+                        <div className="form-group col-3">
+                            <label htmlFor="table_name">Table Name</label>
+                            <input
+                                type="text"
+                                minLength="2"
+                                id="table_name"
+                                name="table_name"
+                                value={table.table_name}
+                                className="form-control"
+                                placeholder="Table Name"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group col-2">
+                            <label htmlFor="capacity">Table Capacity</label>
+                            <input
+                                type="number"
+                                id="capacity"
+                                min="1"
+                                name="capacity"
+                                placeholder="Table capacity"
+                                className="form-control"
+                                value={table.capacity}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        </div>
+                        <button
+                            type="button"
+                            className="btn btn-secondary mr-2"
+                            onClick={() => history.goBack()}
+                        >Cancel</button>
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                        >Submit</button>
+                </fieldset>
+            </form>
+        </div>
+    )
 }
+
+export default CreateTable;
