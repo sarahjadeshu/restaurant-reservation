02a07e14ad0cd666ee0c45c24b9cf12494850404
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-15 01:51:54 -0700

    implemented list function in reservations controller

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,24 @@
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const service = require("./reservations.service");
+const hasProperties = require("../errors/hasProperties");
 
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const { date } = req.query;
+  const { mobile_number } = req.query;
+  let data;
+
+  if (date) {
+    data = await service.listByDate(date);
+  } else if (mobile_number) {
+    data = await service.search(mobile_number);
+  } else {
+    data = await service.list();
+  }
+
+  res.json({ data });
 }
 
 module.exports = {
