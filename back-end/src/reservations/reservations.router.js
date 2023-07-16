2be3e235ad84b0f6b55d787401dd7d5a9c8c65e2
Sarah Shu <sarahjadeshu@gmail.com>
2023-07-16 00:42:21 -0700

    updated reservations router

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -4,9 +4,12 @@
  * @type {Router}
  */
 
-const router = require("express").Router({ mergeParams: true });
+const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
+router.route("/:reservation_id").get(controller.read).put(controller.update).delete(controller.delete).all(methodNotAllowed);
+router.route("/:reservation_id/status").put(controller.updateStatus).all(methodNotAllowed);
 
 module.exports = router;
